e438dda66fab4fc29b4dd0a8ab8b64e7490a855e
Stoiko Ivanov <s.ivanov@proxmox.com>
2025-02-25 16:02:03 +0100

    configuration: options: add admin-mail-from row.
    Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

--- js/SystemOptions.js
+++ js/SystemOptions.js
@@ -75,6 +75,9 @@ Ext.define('PMG.SystemOptions', {
 	me.add_text_row('email', gettext("Administrator EMail"),
 			{ deleteEmpty: true, defaultValue: Proxmox.Utils.noneText });
 
+	me.add_text_row('admin-mail-from', gettext("'From:' for Admin Mail"),
+			{ deleteEmpty: true, defaultValue: Proxmox.Utils.noneText });
+
 	me.add_proxy_row('http_proxy', gettext("HTTP proxy"));
 
 	me.callParent();
