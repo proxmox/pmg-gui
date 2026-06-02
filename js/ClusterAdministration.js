9bc1fb2833e7723b17351d2e1e17739ee558a79e
Thomas Lamprecht <t.lamprecht@proxmox.com>
2026-06-02 15:40:00 +0200

    cluster: assist node joining via copyable join information
    Mirror the convenience already present in Proxmox VE. On an existing
    cluster a new "Join Information" dialog shows the master's address and
    fingerprint together with a base64-encoded blob and a copy button. On
    the new node the join dialog gains an assisted mode that decodes such a
    blob and fills in the peer address and fingerprint as read-only fields,
    with manual entry kept as fallback.
    
    The pasted information is checked against the local node: a blob from a
    different product is rejected, a differing major release warns and a
    differing minor release shows a note.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- js/ClusterAdministration.js
+++ js/ClusterAdministration.js
@@ -45,56 +45,226 @@ Ext.define('PMG.ClusterJoinNodeWindow', {
     onlineHelp: 'pmgcm_join',
 
     title: gettext('Cluster Join'),
-
     width: 800,
 
     method: 'POST',
-
     url: '/config/cluster/join',
+
+    isCreate: true,
+    submitText: gettext('Join'),
+
+    defaultFocus: 'textarea[name=serializedinfo]',
+
+    viewModel: {
+        parent: null,
+        data: {
+            info: {
+                fp: '',
+                ip: '',
+            },
+            hasAssistedInfo: false,
+            localRelease: undefined,
+            versionNotice: '',
+        },
+        formulas: {
+            showClusterFields: (get) => get('hasAssistedInfo') || !get('assistedEntry.checked'),
+            showVersionNotice: (get) => !!get('versionNotice') && get('assistedEntry.checked'),
+        },
+    },
+
+    controller: {
+        xclass: 'Ext.app.ViewController',
+        control: {
+            'textarea[name=serializedinfo]': {
+                change: 'recomputeSerializedInfo',
+                enable: 'resetField',
+            },
+        },
+        init: function () {
+            let me = this;
+            // remember our own release to warn about version differences with the cluster
+            Proxmox.Utils.API2Request({
+                url: '/version',
+                method: 'GET',
+                success: function (response) {
+                    me.getViewModel().set('localRelease', response.result.data.release);
+                },
+                failure: function () {
+                    // best-effort, just skip the version hint if we cannot read our own version
+                },
+            });
+        },
+        resetField: function (field) {
+            field.reset();
+        },
+        checkVersion: function (remoteRelease) {
+            let localRelease = this.getViewModel().get('localRelease');
+            if (!remoteRelease || !localRelease || remoteRelease === localRelease) {
+                return '';
+            }
+            let [remoteMajor] = remoteRelease.split('.');
+            let [localMajor] = localRelease.split('.');
+            if (remoteMajor !== localMajor) {
+                return Ext.String.format(
+                    gettext(
+                        'Warning: the cluster runs Proxmox Mail Gateway {0}, but this node runs {1}. Joining across major versions is not supported.',
+                    ),
+                    remoteRelease,
+                    localRelease,
+                );
+            }
+            return Ext.String.format(
+                gettext('Note: the cluster runs Proxmox Mail Gateway {0}, but this node runs {1}.'),
+                remoteRelease,
+                localRelease,
+            );
+        },
+        recomputeSerializedInfo: function (field, value) {
+            let vm = this.getViewModel();
+
+            if (!this.lookup('assistedEntry').getValue()) {
+                // not in assisted entry mode, nothing to do
+                vm.set('hasAssistedInfo', false);
+                return;
+            }
+
+            // Ext.JSON.decode with the 'safe' flag returns null instead of throwing
+            let joinInfo = Ext.JSON.decode(Ext.util.Base64.decode(value), true);
+
+            let info = { fp: '', ip: '' };
+            let notice = '';
+            field.validityHint = undefined;
+
+            if (!joinInfo || !joinInfo.ip || !joinInfo.fingerprint) {
+                field.valid = false;
+            } else if (joinInfo.product !== 'pmg') {
+                field.valid = false;
+                field.validityHint = gettext(
+                    'This is not the join information of a Proxmox Mail Gateway cluster.',
+                );
+            } else {
+                info = {
+                    ip: joinInfo.ip,
+                    fp: joinInfo.fingerprint,
+                };
+                notice = this.checkVersion(joinInfo.version);
+                field.valid = true;
+            }
+
+            vm.set('info', info);
+            vm.set('hasAssistedInfo', field.valid);
+            vm.set('versionNotice', notice);
+        },
+    },
 
     items: [
         {
+            xtype: 'proxmoxcheckbox',
+            reference: 'assistedEntry',
+            name: 'assistedEntry',
+            itemId: 'assistedEntry',
+            submitValue: false,
+            value: true,
+            autoEl: {
+                tag: 'div',
+                'data-qtip': gettext(
+                    'Select if join information should be extracted from pasted cluster information, deselect for manual entering',
+                ),
+            },
+            boxLabel: gettext(
+                'Assisted join: Paste encoded cluster join information and enter password.',
+            ),
+        },
+        {
+            xtype: 'textarea',
+            name: 'serializedinfo',
+            submitValue: false,
+            allowBlank: false,
+            fieldLabel: gettext('Information'),
+            emptyText: gettext('Paste encoded Cluster Information here'),
+            validator: function (val) {
+                if (val === '') {
+                    return true;
+                }
+                return (
+                    this.valid ||
+                    this.validityHint ||
+                    gettext('Does not seem like a valid encoded Cluster Information!')
+                );
+            },
+            bind: {
+                disabled: '{!assistedEntry.checked}',
+                hidden: '{!assistedEntry.checked}',
+            },
+            value: '',
+        },
+        {
+            xtype: 'displayfield',
+            userCls: 'pmx-hint',
+            hideLabel: true,
+            bind: {
+                value: '{versionNotice}',
+                hidden: '{!showVersionNotice}',
+            },
+        },
+        {
             xtype: 'textfield',
-            fieldLabel: 'IP Address',
+            fieldLabel: gettext('Peer Address'),
+            allowBlank: false,
+            bind: {
+                value: '{info.ip}',
+                readOnly: '{assistedEntry.checked}',
+                hidden: '{!showClusterFields}',
+            },
             name: 'master_ip',
         },
         {
             xtype: 'textfield',
             inputType: 'password',
+            emptyText: gettext("Peer's root password"),
             fieldLabel: gettext('Password'),
+            allowBlank: false,
+            bind: {
+                hidden: '{!showClusterFields}',
+            },
             name: 'password',
         },
         {
             xtype: 'textfield',
             fieldLabel: gettext('Fingerprint'),
+            allowBlank: false,
+            bind: {
+                value: '{info.fp}',
+                readOnly: '{assistedEntry.checked}',
+                hidden: '{!showClusterFields}',
+            },
             name: 'fingerprint',
         },
     ],
 });
 
-Ext.define('PMG.ClusterAddNodeWindow', {
+Ext.define('PMG.ClusterInfoWindow', {
     extend: 'Ext.window.Window',
-    xtype: 'pmgClusterAddNodeWindow',
+    xtype: 'pmgClusterInfoWindow',
     mixins: ['Proxmox.Mixin.CBind'],
 
     width: 800,
-
     modal: true,
+    resizable: false,
 
-    title: gettext('Cluster Join') + ' : ' + gettext('Information'),
+    title: gettext('Cluster Join Information'),
 
-    ipAddress: undefined,
-
-    fingerprint: undefined,
+    joinInfo: {
+        ip: undefined,
+        fingerprint: undefined,
+    },
 
     items: [
         {
             xtype: 'component',
             border: false,
             padding: '10 10 10 10',
-            html: gettext(
-                "Please use the 'Join' button on the node you want to add, using the following IP address and fingerprint.",
-            ),
+            html: gettext('Copy the Join Information here and use it on the node you want to add.'),
         },
         {
             xtype: 'container',
@@ -105,14 +275,49 @@ Ext.define('PMG.ClusterAddNodeWindow', {
                 {
                     xtype: 'textfield',
                     fieldLabel: gettext('IP Address'),
-                    cbind: { value: '{ipAddress}' },
+                    cbind: { value: '{joinInfo.ip}' },
                     editable: false,
                 },
                 {
                     xtype: 'textfield',
                     fieldLabel: gettext('Fingerprint'),
-                    cbind: { value: '{fingerprint}' },
+                    cbind: { value: '{joinInfo.fingerprint}' },
                     editable: false,
+                },
+                {
+                    xtype: 'textarea',
+                    inputId: 'pmgSerializedClusterInfo',
+                    fieldLabel: gettext('Join Information'),
+                    grow: true,
+                    cbind: { joinInfo: '{joinInfo}' },
+                    editable: false,
+                    listeners: {
+                        afterrender: function (field) {
+                            if (!field.joinInfo) {
+                                return;
+                            }
+                            let json = Ext.JSON.encode(field.joinInfo);
+                            field.setValue(Ext.util.Base64.encode(json));
+                        },
+                    },
+                },
+            ],
+        },
+    ],
+    dockedItems: [
+        {
+            dock: 'bottom',
+            xtype: 'toolbar',
+            items: [
+                {
+                    xtype: 'button',
+                    handler: function (b) {
+                        let el = document.getElementById('pmgSerializedClusterInfo');
+                        el.select();
+                        document.execCommand('copy');
+                    },
+                    text: gettext('Copy Information'),
+                    iconCls: 'fa fa-clipboard',
                 },
             ],
         },
@@ -186,22 +391,25 @@ Ext.define('PMG.ClusterAdministration', {
                 },
 
                 onJoin: function () {
-                    var win = Ext.create('PMG.ClusterJoinNodeWindow', {});
-                    win.show();
-                    win.on('destroy', function () {
-                        // fixme: logout
-                    });
+                    Ext.create('PMG.ClusterJoinNodeWindow', {}).show();
                 },
 
-                onAdd: function () {
-                    var vm = this.getViewModel();
+                onClusterInfo: function () {
+                    var view = this.getView();
 
-                    var win = Ext.create('PMG.ClusterAddNodeWindow', {
-                        ipAddress: vm.get('master').get('ip'),
-                        fingerprint: vm.get('master').get('fingerprint'),
+                    Proxmox.Utils.API2Request({
+                        url: '/config/cluster/join',
+                        method: 'GET',
+                        waitMsgTarget: view,
+                        failure: function (response, opts) {
+                            Ext.Msg.alert(gettext('Error'), response.htmlStatus);
+                        },
+                        success: function (response, options) {
+                            Ext.create('PMG.ClusterInfoWindow', {
+                                joinInfo: response.result.data,
+                            }).show();
+                        },
                     });
-
-                    win.show();
                 },
             },
             store: {
@@ -219,9 +427,9 @@ Ext.define('PMG.ClusterAdministration', {
                     },
                 },
                 {
-                    text: gettext('Add'),
-                    reference: 'addButton',
-                    handler: 'onAdd',
+                    text: gettext('Join Information'),
+                    reference: 'clusterInfoButton',
+                    handler: 'onClusterInfo',
                     bind: {
                         disabled: '{!master}',
                     },
